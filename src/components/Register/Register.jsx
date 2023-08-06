d44573f17e26b93835a49b8705690ad58518022b
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-06 12:01:25 +0600

    onChange, onBlur, onSubmit and preventDefault on Simple Form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password);
+    }
+
+    const handleEmailChange = (event) => {
+        const email = event.target.value;
+        console.log(email);
+        setEmail(email);
+    }
+
+    const handlePasswordBlur = (event) => {
+        const password = event.target.value;
+        console.log(password);
+    }
+
     return (
         <div>
-            <input type="email" name="email" id="email" placeholder='Your Email' />
-            <br/>
-            <input type="password" name="password" id="password" placeholder='Your Password' />
-            <br/>
-            <input type="submit" value="Register" />
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' />
+                <br/>
+                <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' />
+                <br/>
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };
